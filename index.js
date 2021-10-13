aee6e2224575cfb7f0978b494e2b6e18b4a7fb8e
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 14:08:52 +0000

    problem

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+*/
