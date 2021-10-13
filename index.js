c9fb14335ac8c7a642a97800916dcc6357bd5f48
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 15:27:55 +0000

    two pointer solution

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ Given a string s, reverse the order of characters in each word within a sentence
 Input: s = "Let's take LeetCode contest"
 Output: "s'teL ekat edoCteeL tsetnoc"
 */
+
+var reverseWords = function(s) {
+    let arr = s.split(' ')
+    for (let i=0; i<arr.length; i++){
+        arr2 = arr[i].split('')
+        let start = 0
+        let end = arr2.length-1
+        while (start <= end){
+            [arr2[start++],arr2[end--]] = [arr2[end],arr2[start]]
+        }
+        arr[i] = arr2.join('')
+    }
+    return arr.join(' ')
+};
